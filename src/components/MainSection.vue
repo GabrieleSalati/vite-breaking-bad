588dc5cab4e1920527c9f60f72597cf1a8f00337
GabrieleSalati <gabriele.salati5@gmail.com>
2023-03-03 13:49:26 +0100

    Basic layout ok

---
+++ src/components/MainSection.vue
@@ -0,0 +1,28 @@
+<script>
+// import MyComponent from "./components/MyComponent.vue";
+
+export default {
+    data() {
+        return {
+
+        }
+    }
+
+    // components: {
+    //   MyComponent,
+    // },
+};
+</script>
+
+<template>
+    <main>
+
+    </main>
+</template>
+
+<style lang="scss" scoped>
+main {
+    background-color: coral;
+    height: 100vh;
+}
+</style>
